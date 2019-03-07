feb8a0056c868c0cf4ce543f01ed23b208665de9
Jasmin <jas.minnoel.22@gmail.com>
2019-03-06 20:55:14 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+        $('.femme').mouseenter(function() { 
+            var id = $(this).attr('id');
+            $('#text_'+id).removeClass('invisible-fff');
+            $(this).popover({
+                html: true,
+                boundary: $('#map-container'),
+                trigger: 'focus',
+                container: 'body'
+            }); 
+        });
+        $('.femme').mouseleave(function() { 
+            var id = $(this).attr('id');
+            $('#text_'+id).addClass('invisible-fff');
+        });
+
+        $('.femme').on('click', function() { 
+            var e = $(this);
+            e.popover({
+                html: true,
+                boundary: $('#map-container'),
+                trigger: 'focus',
+                container: 'body'
+            }); 
+        });
+});
+ 
+// function pop() {
+//     alert('yeah');
+//     $('#' + nom).popover({
+//         html: true,
+//         boundary: $('#map-container'),
+//         trigger: 'focus click',
+//         container: 'body'
+//     });
+// }
+
